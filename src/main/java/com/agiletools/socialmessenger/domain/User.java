0df78f611d55095509cafcf1c1b3210105769149
mmuniz75 <mmunizs1975@gmail.com>
2020-04-28 11:52:20 -0300

    contrutor de usuario

--- src/main/java/com/agiletools/socialmessenger/domain/User.java
+++ src/main/java/com/agiletools/socialmessenger/domain/User.java
@@ -1,13 +1,17 @@
 package com.agiletools.socialmessenger.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @Document("Users")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
