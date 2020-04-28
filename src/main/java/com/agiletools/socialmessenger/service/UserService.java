e11d7998e6f4efbe3b21b2d2c261506a65d9723b
mmuniz75 <mmunizs1975@gmail.com>
2020-04-28 16:53:02 -0300

    tratamento de erros

--- src/main/java/com/agiletools/socialmessenger/service/UserService.java
+++ src/main/java/com/agiletools/socialmessenger/service/UserService.java
@@ -3,6 +3,7 @@ package com.agiletools.socialmessenger.service;
 import com.agiletools.socialmessenger.domain.User;
 import com.agiletools.socialmessenger.exception.APIException;
 import com.agiletools.socialmessenger.exception.ConflictException;
+import com.agiletools.socialmessenger.exception.NotFoundException;
 import com.agiletools.socialmessenger.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,11 +18,12 @@ import javax.validation.Valid;
 @Service
 public class UserService {
 
-    private static final String EMAIL_ALREADY_EXISTS = "%s já está cadastrado";
+    private static final String EMAIL_ALREADY_EXISTS = "Email já está cadastrado";
+    private static final String EMAIL_NOT_FOUND = "%s não cadastrado";
 
     private final UserRepository repository;
 
-    public UserService(UserRepository repository){
+    public UserService(UserRepository repository) {
 
         this.repository = repository;
     }
@@ -34,14 +36,15 @@ public class UserService {
     public Mono<User> addUser(@RequestBody @Valid User user) throws APIException {
 
         return repository.findByEmail(user.getEmail())
-                        .hasElement()
-                        .flatMap(userExits -> {
-                            if (!userExits.booleanValue())
-                                return repository.save(user);
-                            else
-                                return Mono.error(new ConflictException(String.format(EMAIL_ALREADY_EXISTS,user.getEmail())));
-                        })
-                        ;
+                .hasElement()
+                .flatMap(userExits -> {
+                    if (!userExits.booleanValue())
+                        return repository.save(user);
+                    else
+                        return Mono.error(new ConflictException(EMAIL_ALREADY_EXISTS));
+                })
+                ;
 
     }
+
 }
